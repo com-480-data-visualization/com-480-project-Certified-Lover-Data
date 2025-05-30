f203cf316c4a61d6df0393c4a0645f0d2aab0eb6
Samaeltawil <sama.weltawil@gmail.com>
2025-05-30 11:33:26 +0200

    Data Fetching Fix

--- scripts/genre_race.js
+++ scripts/genre_race.js
@@ -101,7 +101,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const containerX = (width + margin.left + margin.right - totalGridW) / 2;
   const containerY = height + margin.top + 80;
 
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
   const monthShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   function showYearsMonths(year, monthStr) {
